865544fe872495a1aa54d6c49fe3524a94d85c3a
carvalho57 <gabrielfeocarvalho57@gmail.com>
2023-10-23 21:07:29 -0300

    feat: adiciona suporte a configurações com .env

--- src/app/Config.php
+++ src/app/Config.php
@@ -6,4 +6,52 @@ namespace App;
 
 class Config
 {
+    private array $db = [];
+    private array $certificado = [];
+    private array $empresa = [];
+
+    public function __construct(array $config)
+    {
+        $this->db = [
+            'username' => $config['DB_USERNAME'],
+            'password' => $config['DB_PASSWORD'],
+            'database' => $config['DB_DATABASE'],
+            'host' => $config['DB_HOST'],
+        ];
+
+        $this->empresa = [
+            'cnpj' => $config['CNPJ'],
+            'uf' => $config['UF'],
+        ];
+
+        $this->certificado = [
+            'path' => $config['CERTIFICATE_PATH'],
+            'password' => $config['CERTIFICATE_PASSWORD'],
+        ];
+    }
+
+    public function db(string $propriedade)
+    {
+        if (!empty($propriedade)) {
+            return $this->db[$propriedade] ?? '';
+        }
+
+        return $this->db;
+    }
+    public function certificado(string $propriedade): string|array
+    {
+        if (!empty($propriedade)) {
+            return $this->db[$propriedade] ?? '';
+        }
+
+        return $this->db;
+    }
+
+    public function empresa(string $propriedade)
+    {
+        if (!empty($propriedade)) {
+            return $this->empresa[$propriedade] ?? '';
+        }
+        return $this->empresa;
+    }
 }
--- src/public/index.php
+++ src/public/index.php
@@ -2,6 +2,13 @@
 
 declare(strict_types=1);
 
+use App\Config;
+
 require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . '/vendor/autoload.php';
 
-phpinfo();
+$env = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$env->load();
+
+$config = new Config($_ENV);
+
+var_dump($config);

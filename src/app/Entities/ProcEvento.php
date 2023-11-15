2fa7021937b4889be23b61c1e320e11b7ad4ee25
carvalho57 <gabrielfeocarvalho57@gmail.com>
2023-11-14 22:56:52 -0300

    feat: implementado metodo processar para o tipo procEvento

--- src/app/Entities/ProcEvento.php
+++ src/app/Entities/ProcEvento.php
@@ -2,17 +2,54 @@
 
 namespace App\Entities;
 
-class ProcEvento extends Entity
+use PDO;
+
+class ProcEvento extends Entity implements DocumentProcessable
 {
     protected int $nsu;
+    protected string $chaveAcesso;
+    protected string $protocolo;
+    private Documento $documento;
 
-    public function fromXML($xml): static
+    public function __construct(Documento $documento)
     {
-        return (new static());
+        parent::__construct();
+        $this->documento = $documento;
+        $this->loadXML($this->documento->extrairConteudo());
     }
 
-    public function processar(Documento $documento): bool
+    public function loadXML(string $xml)
     {
-        return false;
+        $dom = new \DOMDocument();
+        $dom->loadXML($xml);
+
+        $retEvento = $dom->getElementsByTagName('retEvento')->item(0);
+        $infProt = $retEvento->getElementsByTagName('infEvento')->item(0);
+
+        $this->chaveAcesso = $infProt->getElementsByTagName('chNFe')->item(0)->nodeValue;
+        $this->protocolo = $infProt->getElementsByTagName('nProt')->item(0)->nodeValue;
+    }
+
+
+    public function processar(): bool
+    {
+        $statement = $this->pdo()->query("SELECT IDEVENTO FROM EVENTO WHERE CHAVE_ACESSO = {$this->chaveAcesso}");
+        $row = $statement->fetch();
+
+        if ($statement->rowCount() === 0) {
+            return false;
+        }
+
+        try {
+            $updateNFe = 'UPDATE EVENTO SET XML = :xml WHERE IDEVENTO = :id';
+
+            $statement = $this->pdo()->prepare($updateNFe);
+            $statement->bindValue(':xml', $this->documento->conteudo, PDO::PARAM_STR);
+            $statement->bindValue(':id', $row['IDEVENTO'], PDO::PARAM_INT);
+
+            return $statement->execute();
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), $e->getCode());
+        }
     }
 }

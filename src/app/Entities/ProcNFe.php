8aba5acd8c2bd24576c7c40f5e39fc91be72d2b6
carvalho57 <gabrielfeocarvalho57@gmail.com>
2023-11-14 22:57:10 -0300

    feat: implementado metodo processar para o tipo procNFe

--- src/app/Entities/ProcNFe.php
+++ src/app/Entities/ProcNFe.php
@@ -2,17 +2,52 @@
 
 namespace App\Entities;
 
-class ProcNFe extends Entity
+use PDO;
+
+class ProcNFe extends Entity implements DocumentProcessable
 {
     protected int $nsu;
+    protected string $chaveAcesso;
+    protected string $protocolo;
+    private Documento $documento;
 
-    public function fromXML($xml): static
+    public function __construct(Documento $documento)
     {
-        return (new static());
+        parent::__construct();
+        $this->documento = $documento;
+        $this->loadXML($this->documento->extrairConteudo());
     }
 
-    public function processar(Documento $documento): bool
+    public function loadXML(string $xml)
     {
-        return false;
+        $dom = new \DOMDocument();
+        $dom->loadXML($xml);
+
+        $infProt = $dom->getElementsByTagName('infProt')->item(0);
+        $this->chaveAcesso = $infProt->getElementsByTagName('chNFe')->item(0)->nodeValue;
+        $this->protocolo = $infProt->getElementsByTagName('nProt')->item(0)->nodeValue;
+    }
+
+    public function processar(): bool
+    {
+        $statement = $this->pdo()->query("SELECT IDNFE FROM NFE WHERE CHAVE_ACESSO = '{$this->chaveAcesso}'");
+        $row = $statement->fetch();
+
+        if ($statement->rowCount() === 0) {
+            return false;
+        }
+
+        try {
+            $updateNFe = 'UPDATE NFE SET XML = :xml WHERE IDNFE = :id';
+
+            $statement = $this->pdo()->prepare($updateNFe);
+            $statement->bindValue(':xml', $this->documento->conteudo, PDO::PARAM_STR);
+            $statement->bindValue(':id', $row['IDNFE'], PDO::PARAM_INT);
+
+
+            return $statement->execute();
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), $e->getCode());
+        }
     }
 }
